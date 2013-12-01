8edc1244adc8120296a9137d7df72aa5d276a63f
Carlos Interone <carlos@mostlyclicks.com>
2013-11-30 19:03:45 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trhobbies2::Application.config.secret_token = 'f9386fdf0e644e8b5c33dd8c74455a40b8a22eb17d1bf552c11110b2e05a34d0734c3cc8969366b86bb8c2ded5ab734907239803448b1757da3c02c7d8e49914'
